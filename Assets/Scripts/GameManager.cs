183e2626e31deed9d37ecabd26f25cc510219e98
Tayou <tayou@gmx.net>
2020-05-14 18:03:01 +0200

    area data formatted for json

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
 
@@ -21,6 +22,8 @@ public class GameManager : MonoBehaviour {
     public Text radiusInputText;
     public Slider heightSlider;
     public Text heightInputText;
+
+    public Text jsonDataForCurrentObject;
     
     private GameObject camera;
     public GameObject radiusPrefab;
@@ -59,30 +62,36 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Fire2") >= 0.1f || Input.GetAxis("Fire3") >= 0.1f || (tool == 0 && Input.GetAxis("Fire1") >= 0.1f)) {
-            camera.transform.parent.Rotate(new Vector3(
-                                                       lastMousePosition.y - Input.mousePosition.y, 
-                                                       Input.mousePosition.x - lastMousePosition.x, 
-                                                       Input.mousePosition.z - lastMousePosition.z), Space.Self);
-        }
-        lastMousePosition = Input.mousePosition;
+        if (!EventSystem.current.IsPointerOverGameObject()) {
+            if (Input.GetAxis("Fire2") >= 0.1f || Input.GetAxis("Fire3") >= 0.1f || (tool == 0 && Input.GetAxis("Fire1") >= 0.1f)) {
+                camera.transform.parent.Rotate(new Vector3(
+                                                           lastMousePosition.y - Input.mousePosition.y, 
+                                                           Input.mousePosition.x - lastMousePosition.x, 
+                                                           Input.mousePosition.z - lastMousePosition.z), Space.Self);
+            }
+            lastMousePosition = Input.mousePosition;
         
-        if (Input.GetAxis("Fire1") >= 0.1f) {
-            if (tool == 1) {
-                Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, Mathf.Infinity);
-                GameObject newSphere = Instantiate(radiusPrefab, hit.point, new Quaternion());
-                spheres.Add(newSphere);
-                SetActiveObject(newSphere);
-                SetRadiusForActiveObject((int)Math.Floor(worldSize * 0.1f));
-            } else if (tool == 2) {
-                Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, Mathf.Infinity);
-                if (hit.collider.gameObject != null) {
-                    SetActiveObject(hit.collider.gameObject);
+            if (Input.GetAxis("Fire1") >= 0.1f) {
+                if (tool == 1) {
+                    Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, Mathf.Infinity);
+                    if (hit.collider.gameObject != null) {
+                        GameObject newSphere = Instantiate(radiusPrefab, hit.point, new Quaternion());
+                        spheres.Add(newSphere);
+                        SetActiveObject(newSphere);
+                        SetRadiusForActiveObject((int)Math.Floor(worldSize * 0.1f));
+                    }
+                } else if (tool == 2) {
+                    Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hit, Mathf.Infinity);
+                    try {
+                        SetActiveObject(hit.collider.gameObject);
+                    } catch (Exception e) {
+                        Debug.Log("" + e);
+                    }
                 }
             }
-        }
         
-        camera.transform.Translate(Vector3.forward * (Input.mouseScrollDelta.y * 10f), Space.Self);
+            camera.transform.Translate(Vector3.forward * (Input.mouseScrollDelta.y * 10f), Space.Self);
+        }
     }
 
     private void SetActiveObject(GameObject active) {
@@ -91,33 +100,42 @@ public class GameManager : MonoBehaviour {
         }
         activeObject = active;
         activeObject.GetComponent<MeshRenderer>().material = selectedMaterial;
+        WriteObjectToJson(activeObject);
     }
 
     private void SetRadiusForActiveObject(int value) {
         activeObject.transform.localScale = new Vector3(value, value, value);
+        WriteObjectToJson(activeObject);
     }
 
-    public void SetHeightInputFromSlider() {
-        heightInputText.text = heightSlider.value.ToString();
+    private void SetHeightForActiveObject(int value) {
+        activeObject.GetComponent<HeightContainer>().height = value;
+        WriteObjectToJson(activeObject);
     }
 
-    public void SetHeightSliderFromText() {
+    public void SetHeightInputFromSlider(float value) {
+        heightInputText.text = value.ToString();
+        SetHeightForActiveObject((int)value);
+    }
+
+    public void SetHeightSliderFromText(String value) {
         try {
-            heightSlider.value = float.Parse(heightInputText.text);
+            heightSlider.SetValueWithoutNotify(int.Parse(value));
+            SetHeightForActiveObject(int.Parse(value));
         } catch (Exception e) {
             Debug.Log("The String was not valid:\n" + e);
         }
     }
 
-    public void SetRadiusInputFromSlider() {
-        int radius = (int)Math.Floor(radiusSlider.value);
+    public void SetRadiusInputFromSlider(float value) {
+        int radius = (int)value;
         radiusInputText.text = radius.ToString();
         SetRadiusForActiveObject(radius);
     }
 
-    public void SetRadiusSliderFromText() {
+    public void SetRadiusSliderFromText(String value) {
         try {
-            int radius = int.Parse(radiusInputText.text);
+            int radius = int.Parse(value);
             radiusSlider.SetValueWithoutNotify(radius);
             SetRadiusForActiveObject(radius);
         } catch (Exception e) {
@@ -129,5 +147,20 @@ public class GameManager : MonoBehaviour {
         planet.transform.localScale = new Vector3(worldSize, worldSize, worldSize);
         camera.transform.position = new Vector3(0, 0, (float)(-worldSize*1.2));
         camera.transform.rotation = new Quaternion();
+    }
+
+    private String WriteObjectToJson(GameObject sphere) {
+        var position = sphere.transform.position;
+        String outString = "{\n" +
+                           "\t\"adjustment\": " + sphere.GetComponent<HeightContainer>().height + ",\n" +
+                           "\t\"radius\": " + sphere.transform.localScale.x + ",\n" +
+                           "\t\"pos\": [\n" +
+                           "\t\t" + position.x + ",\n" +
+                           "\t\t" + position.y + ",\n" +
+                           "\t\t" + position.z + "\n" +
+                           "\t]\n" +
+                           "}\n";
+        jsonDataForCurrentObject.text = outString;
+        return outString;
     }
 }
