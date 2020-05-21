cfd1914cfe4653cbaa5190bc4bb8c6e06cbfe61e
Tayou <tayou@gmx.net>
2020-05-21 16:42:44 +0200

    added sample system

---
+++ Assets/sampleSystem.pas
@@ -0,0 +1,86 @@
+{
+    "name": "-- empty system --",
+    "description": "Created using Supra PAT World Editor",
+    "creator": "Supra PAT World Editor",
+    "version": "0.0",
+    "planets": [
+        {
+            "name": "-- empty planet --",
+            "mass": 20000,
+            "position_x": 39800.0,
+            "position_y": 0.0,
+            "velocity_x": 0.0,
+            "velocity_y": -112.0,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "respawn": false,
+            "start_destroyed": false,
+            "min_spawn_delay": 0,
+            "max_spawn_delay": 0,
+            "planetCSG": [],
+            "metal_spots": [],
+            "landing_zones": {
+                "list": [],
+                "rules": []
+            },
+            "planet": {
+                "seed": 0,
+                "radius": 500,
+                "heightRange": 35,
+                "waterHeight": 34,
+                "waterDepth": 100,
+                "temperature": 50,
+                "metalDensity": 50,
+                "metalClusters": 50,
+                "metalSpotLimit": -1,
+                "biomeScale": 50,
+                "biome": "earth",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 2,
+                "landingZonesPerArmy": 0,
+                "landingZoneSize": 0,
+                "heightAdjustments": []
+            }
+        },
+        {
+            "name": "-- empty planet 2 --",
+            "mass": 20000,
+            "position_x": 59800.0,
+            "position_y": 0.0,
+            "velocity_x": 0.0,
+            "velocity_y": -112.0,
+            "required_thrust_to_move": 0,
+            "starting_planet": true,
+            "respawn": false,
+            "start_destroyed": false,
+            "min_spawn_delay": 0,
+            "max_spawn_delay": 0,
+            "planetCSG": [],
+            "metal_spots": [],
+            "landing_zones": {
+                "list": [],
+                "rules": []
+            },
+            "planet": {
+                "seed": 0,
+                "radius": 900,
+                "heightRange": 35,
+                "waterHeight": 34,
+                "waterDepth": 100,
+                "temperature": 50,
+                "metalDensity": 50,
+                "metalClusters": 50,
+                "metalSpotLimit": -1,
+                "biomeScale": 50,
+                "biome": "earth",
+                "symmetricalMetal": false,
+                "symmetricalStarts": false,
+                "numArmies": 2,
+                "landingZonesPerArmy": 0,
+                "landingZoneSize": 0,
+                "heightAdjustments": []
+            }
+        }
+    ]
+}
